a3f9fb1cb514ab43050b0d741baf62574b8b38e8
Johannes Hönger <johannes.hoenger@sap.com>
2020-06-05 20:40:24 +0200

    added new maps project

---
+++ 04 - map/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	colors := map[string]string{
+		"red":   "#ff00000",
+		"green": "#008000",
+	}
+
+	var colors_alt map[string]string
+
+	fmt.Println(colors)
+	fmt.Println(colors_alt)
+}
